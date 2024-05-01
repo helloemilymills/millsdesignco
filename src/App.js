96f58bbc1e2de89dccc5cd2606e4ab85133c2a19
helloemilymills <helloemilymills@gmail.com>
2024-05-01 10:26:30 +1000

    commented out unused vars

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Nav from './components/Nav.js';
-import VideoPanel from './components/VideoPanel.js';
-import BrandStoryPanel from './components/BrandStoryPanel.js';
-import ValuesPanel from './components/ValuesPanel.js';
-import CreatorRecsPanel from './components/CreatorRecsPanel.js';
-import SocialsPanel from './components/SocialsPanel.js';
-import Footer from './components/Footer.js';
+// import Nav from './components/Nav.js';
+// import VideoPanel from './components/VideoPanel.js';
+// import BrandStoryPanel from './components/BrandStoryPanel.js';
+// import ValuesPanel from './components/ValuesPanel.js';
+// import CreatorRecsPanel from './components/CreatorRecsPanel.js';
+// import SocialsPanel from './components/SocialsPanel.js';
+// import Footer from './components/Footer.js';
 import ComingSoon from './components/ComingSoon.js';
 
 function App() {
