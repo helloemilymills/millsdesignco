6a29a723fe1ba20532732a1b7b83b9b01b27f85a
helloemilymills <helloemilymills@gmail.com>
2024-05-23 22:42:18 +1000

    image fix in brand story, creator recs and socials panel adjusted for desktop

--- src/components/CreatorRecsPanel.js
+++ src/components/CreatorRecsPanel.js
@@ -9,9 +9,11 @@ export default function CreatorRecsPanel() {
     return (
       <>
         <div id='creatorSpotlightContainer' className='consistentPadding'>
-          <h2>Creator Spotlight</h2>
-          <div className='whiteBar'></div>
-          <p>There is always more to learn, and everyone who shares their knowledge does so with a unique perspective. Here are a few creators who inspire me and share valuable design tips. Give them a follow if you like.</p>
+          <div id='creatorTextContainer'>
+            <h2>Creator Spotlight</h2>
+            <div className='whiteBar'></div>
+            <p>There is always more to learn, and everyone who shares their knowledge does so with a unique perspective. Here are a few creators who inspire me and share valuable design tips. Give them a follow if you like.</p>
+          </div>
           <div id='creatorsContainer'>
             <a href='https://www.instagram.com/barnardco' target='_blank' rel='noopener noreferrer'>
               <div className='creator'>
