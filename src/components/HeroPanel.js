65f3aabfe357e35127d328e19e6a5a38e74acd0e
helloemilymills <helloemilymills@gmail.com>
2024-05-26 22:14:14 +1000

    fixed link issue with carousel covers

--- src/components/HeroPanel.js
+++ src/components/HeroPanel.js
@@ -11,6 +11,10 @@ export default function HeroPanel() {
   const [featureImageSrc, setFeatureImageSrc] = useState(failingFast);
   const [rightImageSrc, setRightImageSrc] = useState(photoshopCropTool);
 
+  const [leftImageLink, setLeftImageLink] = useState('https://www.tiktok.com/@millsdesign.co');
+  const [featureImageLink, setFeatureImageLink] = useState('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=en');
+  const [rightImageLink, setRightImageLink] = useState('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
+
 //clicking the arrows changes the img src:
 // config 1 = prototypes, failing fast, crop tool
 // config 2 = failing fast, crop tool, prototypes
@@ -20,18 +24,27 @@ export default function HeroPanel() {
   function moveCarouselImageLeft() {
     if (configuration === 1) {
       setLeftImageSrc(photoshopCropTool);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setFeatureImageSrc(figmaPrototypes);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co');
       setRightImageSrc(failingFast);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setConfiguration(3);
     } else if (configuration === 2) {
       setLeftImageSrc(figmaPrototypes);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co');
       setFeatureImageSrc(failingFast);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setRightImageSrc(photoshopCropTool);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setConfiguration(1);
     } else if (configuration === 3) {
       setLeftImageSrc(failingFast);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setFeatureImageSrc(photoshopCropTool);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setRightImageSrc(figmaPrototypes);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co');
       setConfiguration(2);
     }
   };
@@ -39,18 +52,27 @@ export default function HeroPanel() {
   function moveCarouselImageRight() {
     if (configuration === 1) {
       setLeftImageSrc(failingFast);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setFeatureImageSrc(photoshopCropTool);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setRightImageSrc(figmaPrototypes);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co');
       setConfiguration(2);
     } else if (configuration === 2) {
       setLeftImageSrc(photoshopCropTool);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setFeatureImageSrc(figmaPrototypes);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co');
       setRightImageSrc(failingFast);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setConfiguration(3);
     } else if (configuration === 3) {
       setLeftImageSrc(figmaPrototypes);
+      setLeftImageLink('https://www.tiktok.com/@millsdesign.co');
       setFeatureImageSrc(failingFast);
+      setFeatureImageLink('https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=e');
       setRightImageSrc(photoshopCropTool);
+      setRightImageLink('https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en');
       setConfiguration(1);
     }
   }
@@ -70,13 +92,13 @@ export default function HeroPanel() {
             <div id='carouselArrowLeft' onClick={() => moveCarouselImageLeft()}></div>
             <img id='hiddenImage' src={figmaPrototypes} alt='A cover design for a TikTok tutorial relating to graphic design.'/>
             <div id='carouselImagesContainer'>
-              <a href='https://www.tiktok.com/@millsdesign.co' target='_blank' rel='noopener noreferrer'>
+              <a href={leftImageLink} target='_blank' rel='noopener noreferrer'>
                 <img className='carouselImage leftImage' src={leftImageSrc} alt='A cover design for a TikTok tutorial relating to graphic design.'/>
               </a>
-              <a href='https://www.tiktok.com/@millsdesign.co/video/7370897206695595282?lang=en' target='_blank' rel='noopener noreferrer'>
+              <a href={featureImageLink} target='_blank' rel='noopener noreferrer'>
                 <img className='carouselImage featuredImage' src={featureImageSrc} alt='A cover design for a TikTok tutorial relating to graphic design.'/>
               </a>
-              <a href='https://www.tiktok.com/@millsdesign.co/video/7364749904431959314?lang=en' target='_blank' rel='noopener noreferrer'>
+              <a href={rightImageLink} target='_blank' rel='noopener noreferrer'>
                 <img className='carouselImage rightImage' src={rightImageSrc} alt='A cover design for a TikTok tutorial relating to graphic design.'/>
               </a>
             </div>
